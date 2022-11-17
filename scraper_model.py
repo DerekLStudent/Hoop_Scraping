98a32b5b64bcc8ee609d4a6235c023c580074328
DerekLStudent <derek.lu@colorado.edu>
2022-11-17 15:24:22 -0700

    Getting correct response object from scraping

--- scraper_model.py
+++ scraper_model.py
@@ -1,4 +1,6 @@
 import requests
+import re
+import constants
 
 class scraper_model:
     def __init__(self):
@@ -6,6 +8,6 @@ class scraper_model:
         self.playerIterator = None
         self.roster = None
     def scrape_team(self, team_name, team_year):
-        URL = "https://www.basketball-reference.com/teams/{}/{}.html"
+        URL = "https://www.basketball-reference.com/teams/{}/{}.html".format(constants.TEAM_NAME_ACRONYM[team_name], team_year)
         r = requests.get(URL)
-        print(r.content)
+        print(re.search('Brad Stevens', r.text))
