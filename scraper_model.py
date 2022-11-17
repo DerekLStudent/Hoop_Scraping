2e5fed93423bee30954d370bfc04d0a3072f5ca0
DerekLStudent <derek.lu@colorado.edu>
2022-11-17 16:28:18 -0700

    singleton scraper model

--- scraper_model.py
+++ scraper_model.py
@@ -3,11 +3,15 @@ import re
 import constants
 
 class scraper_model:
+    def __new__(cls):
+    #Singleton
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(scraper_model, cls).__new__(cls)
+        return cls.instance
     def __init__(self):
-        self.cur_data = {}
         self.playerIterator = None
         self.roster = None
+        self.players = []
     def scrape_team(self, team_name, team_year):
         URL = "https://www.basketball-reference.com/teams/{}/{}.html".format(constants.TEAM_NAME_ACRONYM[team_name], team_year)
         r = requests.get(URL)
-        print(re.search('Brad Stevens', r.text))
